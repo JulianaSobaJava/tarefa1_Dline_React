da43dd5a0fc467b0f2729b9ca57c879005f39827
JulianaSobaJava <julianasobajava23@gmail.com>
2022-03-14 04:50:53 +0100

    first commit

---
+++ src/components/header/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./header.scss";
+
+// import { Container } from './styles';
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <ul></ul>
+      </nav>
+    </header>
+  );
+}
---
+++ src/components/header/navLinkList.js
@@ -0,0 +1,26 @@
+export const NavLinkItem = [
+  {
+    id: "1",
+    tittle: "Home",
+  },
+  {
+    id: "2",
+    tittle: "About",
+  },
+  {
+    id: "3",
+    tittle: "Service",
+  },
+  {
+    id: "4",
+    tittle: "New",
+  },
+  {
+    id: "5",
+    tittle: "Blog",
+  },
+  {
+    id: "6",
+    tittle: "Contact",
+  },
+];
